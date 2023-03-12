14f50ff59127167f6aebcc4084a00d7a3a9d581f
sergey.danusevich <vector-56@yandex.ru>
2023-03-12 21:20:59 +0300

    feat: add animation icon open-close (Sun, Mar 12, 2023 23:20:59 PM)

--- cssBayan/script.js
+++ cssBayan/script.js
@@ -7,9 +7,16 @@ function toggleClass() {
     rotate.classList.toggle('rotate')
 }
 
+document.addEventListener('click', function(event) {
+    let id = event.target.dataset.toggleId;
+    if (!id) return;
+
+    let elem = document.getElementById(id);
+
+    elem.hidden = !elem.hidden;
+  });
+
 openClose.addEventListener('click', toggleClass)
-
-
 
 const container = document.querySelector('.container')
 const showHide = document.querySelectorAll('.open-close-icon')
