b899a546762ac6580be9fe51ade4829f3bb93abc
sergey.danusevich <vector-56@yandex.ru>
2023-03-12 21:29:25 +0300

    refactor: refactoring code in script.js (Sun, Mar 12, 2023 21:29:25 PM)

--- cssBayan/script.js
+++ cssBayan/script.js
@@ -1,32 +1,18 @@
 const btn = document.querySelector('.image')
 const openClose = document.querySelector('.content-block')
 const rotate = document.querySelector('.open-close-icon')
-
 function toggleClass() {
     btn.classList.toggle('hide')
     rotate.classList.toggle('rotate')
 }
-
-document.addEventListener('click', function(event) {
-    let id = event.target.dataset.toggleId;
-    if (!id) return;
-
-    let elem = document.getElementById(id);
-
-    elem.hidden = !elem.hidden;
-  });
-
 openClose.addEventListener('click', toggleClass)
-
 const container = document.querySelector('.container')
 const showHide = document.querySelectorAll('.open-close-icon')
-
 function overMouse () {
     for (let i = 0; i < showHide.length; i++) {
         showHide[i].classList.toggle('show')
     }
 }
-
 container.addEventListener('mouseover', overMouse)
 container.addEventListener('mouseout', overMouse)
 
